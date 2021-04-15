8113df4f654ec8e62354a05a71eb45577e40abf0
Facu <faqqu.uri@gmail.com>
2021-04-15 15:46:21 -0300

    :tada: init api

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// load the environment variables from the .env file
+dotenv.config({
+  path: '.env',
+});
+
+// initialize server
+const server = express();
+
+// make server listen on some port
+const port = process.env.APP_PORT ?? 3000;
+server.listen(port, () => console.log(`> Listening on port ${port}`));
